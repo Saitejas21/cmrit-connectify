65f790e0105393ff86a086b07ed248d01ada2b3f
Sai Tejas A R <46528244+Saitejas21@users.noreply.github.com>
2023-05-06 05:02:13 +0530

    feat: post username , profile username hooked db

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -3,9 +3,22 @@ import Topbar from "../../components/topbar/TopBar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Profile() {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const[user,setUser] = useState({})
+
+  useEffect(()=>{
+    const fetchUser = async () =>{
+      const res = await axios.get(`/users?username=sai`);
+      setUser(res.data)
+    };
+   fetchUser();
+  },[]);
+
+
 
   return (
     <>
@@ -27,8 +40,8 @@ export default function Profile() {
               />
             </div>
             <div className="profileInfo">
-                <h4 className="profileInfoName">Sai Tejas A R</h4>
-                <span className="profileInfoDesc">Hello my friends!</span>
+                <h4 className="profileInfoName">{user.username}</h4>
+                <span className="profileInfoDesc">{user.desc}</span>
             </div>
           </div>
           <div className="profileRightBottom">
