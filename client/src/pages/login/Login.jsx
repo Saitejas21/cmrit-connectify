f2debf2201400a5c030ae53bc3b138bd64c5b817
Sai Tejas A R <46528244+Saitejas21@users.noreply.github.com>
2023-05-06 06:13:37 +0530

    feat: login page ready for hook

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,6 +1,15 @@
 import "./login.css";
+import { useRef } from "react";
 
 export default function Login() {
+
+const email = useRef();
+const password = useRef();
+const handleClick = (e) =>{
+  e.preventDefault();
+  console.log(email.current.value);
+};
+
   return (
     <div className="login">
         
@@ -14,15 +23,15 @@ export default function Login() {
           <span className="loginDesc">
            The CMRITIANS Official Social Hub.
           </span>
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
+          <form className="loginBox" onSubmit= { handleClick }>
+            <input placeholder="Email" type="email" required className="loginInput" ref={email} />
+            <input placeholder="Password" type="password" required minLength="6" className="loginInput" ref={password} />
             <button className="loginButton">Log In</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
               Create a New Account
             </button>
-          </div>
+          </form>
           </div>
         </div>
       
