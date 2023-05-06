1c03e8529a5ea1203049b19991dfbf9473bdf97d
Sai Tejas A R <46528244+Saitejas21@users.noreply.github.com>
2023-05-06 05:35:07 +0530

    feat: hooked username from url to redirect

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -5,18 +5,20 @@ import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router";
 
 export default function Profile() {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const[user,setUser] = useState({})
+  const username = useParams().username; 
 
   useEffect(()=>{
     const fetchUser = async () =>{
-      const res = await axios.get(`/users?username=sai`);
+      const res = await axios.get(`/users?username=${username}`);
       setUser(res.data)
     };
    fetchUser();
-  },[]);
+  },[username]);
 
 
 
@@ -45,7 +47,7 @@ export default function Profile() {
             </div>
           </div>
           <div className="profileRightBottom">
-            <Feed username="sai"/>
+            <Feed username={username}/>
             <Rightbar user={user}/>
           </div>
         </div>
